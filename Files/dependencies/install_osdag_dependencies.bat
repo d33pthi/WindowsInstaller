519601c6aba41cb820feca542f27c186270b9885
d33pthi <51116024+d33pthi@users.noreply.github.com>
2020-08-02 11:50:21 +0530

    Add files via upload

--- Files/dependencies/install_osdag_dependencies.bat
+++ Files/dependencies/install_osdag_dependencies.bat
@@ -62,7 +62,8 @@ call pip install .\PyLaTeX-1.3.1.tar.gz
 call pip install .\pdflatex-0.1.3.tar.gz
 call pip install .\keyring-21.2.1.tar.gz
 call pip install .\PyGithub-1.51.tar.gz
-call pip install .\pandas-1.0.5.tar
+call pip install .\pandas-1.0.5-cp37-cp37m-win_amd64.whl
+call pip install .\pynput-1.6.8-py2.py3-none-any.whl
 call conda install .\pythonocc-core-0.18.2-py37h24bf2e0_281.tar.bz2
 call conda install .\oce-0.18.3-vc14_3.tar.bz2
 call conda install .\smesh-6.7.6-vc14_7.tar.bz2
